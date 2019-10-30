e259e4bd03f973806bd11a4ba2c43aa254d4a623
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2019-10-30 15:38:09 -0500

    ERROR DE PROYECTO BASE ---- CORRIGIENDO

--- src/main/java/edu/eci/cvds/guice/GuiceContextListener.java
+++ src/main/java/edu/eci/cvds/guice/GuiceContextListener.java
@@ -1,53 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.eci.cvds.guice;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import edu.eci.cvds.sampleprj.dao.TipoItemDAO;
-import edu.eci.cvds.sampleprj.dao.mybatis.MyBatisTipoItemDAO;
 import org.mybatis.guice.XMLMyBatisModule;
 import org.mybatis.guice.datasource.helper.JdbcHelper;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import edu.eci.cvds.sampleprj.dao.ClienteDAO;
-import edu.eci.cvds.sampleprj.dao.ItemDAO;
-import edu.eci.cvds.sampleprj.dao.mybatis.MyBATISItemDAO;
-import edu.eci.cvds.sampleprj.dao.mybatis.MyBatisClienteDAO;
-import edu.eci.cvds.samples.services.ServiciosAlquiler;
-import edu.eci.cvds.samples.services.impl.ServiciosAlquilerItemsImpl;
+
+import edu.eci.cvds.persistence.ComputerDAO;
+import edu.eci.cvds.persistence.LaboratoryDAO;
+import edu.eci.cvds.persistence.mybatisimpl.MyBatisComputerDAO;
+import edu.eci.cvds.persistence.mybatisimpl.MyBatisLaboratoryDAO;
+import edu.eci.cvds.services.LaboratoryServices;
+import edu.eci.cvds.services.impl.LaboratoryServicesImpl;
+
 
 public class GuiceContextListener implements ServletContextListener {
-    @Override
-    public void contextDestroyed(ServletContextEvent servletContextEvent) {
-        ServletContext servletContext = servletContextEvent.getServletContext();
-        servletContext.removeAttribute(Injector.class.getName());
-    }
-    
-    @Override
-    public void contextInitialized(ServletContextEvent servletContextEvent) {
-        Injector injector = Guice.createInjector(new XMLMyBatisModule() {
-            @Override
-            protected void initialize() {
-                install(JdbcHelper.MySQL);
-                setEnvironmentId("development");
-                setClassPathResource("mybatis-config.xml");
 
-                // TODO Add service class associated to Stub implementation
-                bind(ItemDAO.class).to(MyBATISItemDAO.class);
-                bind(ClienteDAO.class).to(MyBatisClienteDAO.class);
-                bind(TipoItemDAO.class).to(MyBatisTipoItemDAO.class);
-                bind(ServiciosAlquiler.class).to(ServiciosAlquilerItemsImpl.class);
-                
-                
-            }
-        });
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+		ServletContext servletContext = servletContextEvent.getServletContext();
+		servletContext.removeAttribute(Injector.class.getName());
+	}
 
-        servletContextEvent.getServletContext().setAttribute(Injector.class.getName(), injector);
-    }
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+		Injector injector = Guice.createInjector(new XMLMyBatisModule() {
+			@Override
+			protected void initialize() {
+
+				install(JdbcHelper.MySQL);
+				setEnvironmentId("development");
+				setClassPathResource("mybatis-config.xml");
+
+				// Laboratories
+                bind(LaboratoryServices.class).to(LaboratoryServicesImpl.class);
+                bind(ComputerDAO.class).to(MyBatisComputerDAO.class);
+				// Computers
+                bind(LaboratoryDAO.class).to(MyBatisLaboratoryDAO.class);
+			}
+		}
+
+		);
+
+		ServletContext servletContext = servletContextEvent.getServletContext();
+		servletContext.setAttribute(Injector.class.getName(), injector);
+	}
+
 }
